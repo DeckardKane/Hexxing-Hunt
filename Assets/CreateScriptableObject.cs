c485c6b0a717154a06a95591419d303a66376cf6
Peter <pcturnbull7@gmail.com>
2019-11-12 19:21:11 -0500

    Added two scriptable objects, GameStatus and Timer

---
+++ Assets/CreateScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+// path of editor menu from we can create the instance of this scriptable objects
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/MyScriptableObject", order = 1)]
+public class MyScriptable : ScriptableObject
+{
+    public string Name;
+    public float Value;
+}
